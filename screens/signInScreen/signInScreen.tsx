b9ad7f33c4acd7a99ea89df7faf92962cf3a277d
Shashwath Kamath <kamathsh91@gmail.com>
2024-10-19 10:12:28 -0400

    initial commit

---
+++ screens/signInScreen/signInScreen.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+
+const SignInScreen = () => {
+
+    async function onAppleButtonPress() {
+
+    }
+
+    return <View>
+        <Text>Hello</Text>
+    </View>
+}
+
+export default SignInScreen;
