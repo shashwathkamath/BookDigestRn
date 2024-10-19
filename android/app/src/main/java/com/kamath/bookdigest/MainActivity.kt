377b33c04e5e36461c4f7ea1290283400ec21249
Shashwath Kamath <kamathsh91@gmail.com>
2024-10-19 14:39:27 -0400

    bundle id changed

--- android/app/src/main/java/com/kamath/bookdigest/MainActivity.kt
+++ android/app/src/main/java/com/kamath/bookdigest/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bookdigest
+package com.kamath.bookdigest
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "BookDigest"
+  override fun getMainComponentName(): String = "BookB"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
